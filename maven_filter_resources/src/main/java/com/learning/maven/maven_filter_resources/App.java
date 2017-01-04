b60036dc54f03c062ecf58e39106a6cbf8cd4afb
SalahAmine <amin2sellami@gmail.com>
2017-01-04 10:28:15 +0100

    add new maven use

---
+++ maven_filter_resources/src/main/java/com/learning/maven/maven_filter_resources/App.java
@@ -0,0 +1,20 @@
+package com.learning.maven.maven_filter_resources;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	final static Logger log = Logger.getLogger(App.class);
+    public static void main( String[] args )
+    {
+
+    		log.error("this is an error message");
+    		log.debug("this is a debug message");
+
+    }
+}
